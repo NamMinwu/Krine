b0c830552784e0bd40f31f383ea6d76e84ca865f
NamMinwu <minwoo000625@naver.com>
2026-08-20 13:27:35 +0900

    chore: Spring Boot 백엔드 스캐폴드 (Gradle, Lombok, H2 file)

---
+++ backend/src/main/java/com/krine/KrineApplication.java
@@ -0,0 +1,11 @@
+package com.krine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KrineApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(KrineApplication.class, args);
+    }
+}
---
+++ backend/src/test/java/com/krine/KrineApplicationTest.java
@@ -0,0 +1,10 @@
+package com.krine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KrineApplicationTest {
+    @Test
+    void contextLoads() {}
+}
